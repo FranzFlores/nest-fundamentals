cd463f2de0eaae539c1c527e4b2647b8a8564d8e
Franz Flores <andresfloresgallardo@gmail.com>
2024-05-28 21:59:14 -0500

    Métodos normales y getters dentro de una clase

--- typescript-intro/src/fundamentals/03-clasess.ts
+++ typescript-intro/src/fundamentals/03-clasess.ts
@@ -1,11 +1,26 @@
 export class Person {
-    public readonly id: number;
-    public name: string;
 
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
+    constructor(
+        public readonly id: number,
+        public name: string,
+        // public imageUrl: string
+    ) { }
+
+    // método getter
+    get imageUrl(): string {
+        return `http://people.com/${this.id}.jpg`;
+    }
+
+    public work() {
+        console.log(`La persona ${this.name} está trabajando`);
+        this.sleep();
+    }
+    
+    // método privado
+    private sleep() {
+        console.log('La persona está dormida');
     }
 }
 
 export const employee = new Person(1, 'Franz');
+console.log(employee);
