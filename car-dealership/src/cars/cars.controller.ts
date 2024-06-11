f4f20d9dcb78afa0035e1a991bcd8782492ba6c4
Franz Flores <andresfloresgallardo@gmail.com>
2024-06-10 22:20:42 -0500

    Creación de controlador y servicio

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('/cars')
 export class CarsController {
+    constructor(
+        private readonly carsService: CarsService
+    ) {}
 
-    @Get('')
+    @Get('/all')
     getAllCars() {
-        return ['Toyota', 'Chevrolet'];
+        return this.carsService.findAll();
+    }
+
+    @Get('/:id')
+    getCarById(@Param('id', ParseIntPipe) id: number) {
+        return this.carsService.findOneById(id);
     }
 }
