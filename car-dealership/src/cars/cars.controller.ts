281f239f8487b9841d531d2f095ce2d0f9d6b008
Franz Flores <andresfloresgallardo@gmail.com>
2024-06-11 22:00:59 -0500

    Tipos de controlador. POST, PATCH, DELETE

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('/cars')
 export class CarsController {
     constructor(
         private readonly carsService: CarsService
-    ) {}
+    ) { }
+
+    @Post('/create')
+    createCar(@Body() body: any) {
+        return body;
+    }
 
     @Get('/all')
     getAllCars() {
@@ -15,5 +20,15 @@ export class CarsController {
     @Get('/:id')
     getCarById(@Param('id', ParseIntPipe) id: number) {
         return this.carsService.findOneById(id);
+    }
+
+    @Patch('/update/:id')
+    updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+        return { id: id, body: body };
+    }
+
+    @Delete('/delete/:id')
+    deleteCar(@Param('id', ParseIntPipe) id: number) {
+        return { id: id };
     }
 }
