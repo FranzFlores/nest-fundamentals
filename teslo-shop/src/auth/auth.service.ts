9525a6e80b591d6e3c6196f2fb22efccedb248ed
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-19 16:49:57 -0500

    Módulo de usuarios y recurso para la creacion de usuarios

---
+++ teslo-shop/src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userDto } = createUserDto;
+      const user = this.userRepository.create({
+        ...userDto,
+        password: bcrypt.hashSync(password, 10)
+      });
+      
+      await this.userRepository.save(user);
+
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+
+  private handleDBErrors(error: any) {
+    if (error.code == '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    throw new InternalServerErrorException('Error de servidor')
+  }
+}
